1de0db32465e10c778dfcdd9d728a33e5fed9e7f
Cencio Peravolos <cencioperavolos@yahoo.it>
2022-12-03 19:40:22 +0100

    commit per azure

--- src/models/user.js
+++ src/models/user.js
@@ -1,58 +1,65 @@
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
-var userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 3,
-    maxlength: 15
+var userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+      maxlength: 15,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+      maxlength: 15,
+    },
+    surname: {
+      type: String,
+      trim: true,
+      maxlength: 15,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      default: function () {
+        return this.email;
+      },
+    },
+    password: {
+      type: String,
+      trim: true,
+      minlength: 8,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 3,
-    maxlength: 15
-  },
-  surname: {
-    type: String,
-    trim: true,
-    maxlength: 15
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false
-  },
-  isVerified: {
-    type: Boolean,
-    default: false
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    required: true,
-    unique: true
-  },
-  username: {
-    type: String,
-    default: function () { return this.email }
-  },
-  password: {
-    type: String,
-    trim: true,
-    minlength: 8,
-    required: true
-   }
-}, {
-  timestamps: true
-})
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.virtual('fullName').get(function () {
-  let fn = `${this.firstName} ${this.lastName}`
-  if (this.surname.length > 0) { fn = fn.concat(' "' + this.surname + '"') }
-  return fn
-})
+userSchema.virtual("fullName").get(function () {
+  let fn = `${this.firstName} ${this.lastName}`;
+  if (this.surname.length > 0) {
+    fn = fn.concat(' "' + this.surname + '"');
+  }
+  return fn;
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
--- src/routers/expressionsRoute.js
+++ src/routers/expressionsRoute.js
@@ -2,12 +2,14 @@
 
 const express = require('express')
 const Expression = require('../models/expression')
-const isExpressionOwnerOrAdmin = require('../middleware').isExpressionOwnerOrAdmin
+const isExpressionOwnerOrAdmin =
+  require('../middleware').isExpressionOwnerOrAdmin
 const isLoggedIn = require('../middleware').isLoggedIn
 const router = new express.Router()
 
-function diacriticSensitiveRegex (string = '') {
-  return string.replace(/a/g, '[a,á,à,ä]')
+function diacriticSensitiveRegex(string = '') {
+  return string
+    .replace(/a/g, '[a,á,à,ä]')
     .replace(/e/g, '[e,é,ë]')
     .replace(/i/g, '[i,í,ï]')
     .replace(/o/g, '[o,ó,ö,ò]')
@@ -18,7 +20,7 @@ function diacriticSensitiveRegex (string = '') {
 router.get('/search', async (req, res, next) => {
   const perPage = 5
   // var page = req.param('page') || 0
-  const page = (isNaN(req.query.page)) ? 0 : +req.query.page - 1 // default page to 1
+  const page = isNaN(req.query.page) ? 0 : +req.query.page - 1 // default page to 1
 
   let expressions
   let count
@@ -26,22 +28,30 @@ router.get('/search', async (req, res, next) => {
   try {
     if (req.user && req.user.isAdmin) {
       count = await Expression.find({
-        clautano: { $regex: ricerca, $options: 'i' }
+        clautano: { $regex: ricerca, $options: 'i' },
       }).count()
       expressions = await Expression.find({
-        clautano: { $regex: ricerca, $options: 'i' }
-      }).limit(perPage)
+        clautano: { $regex: ricerca, $options: 'i' },
+      })
+        .limit(perPage)
         .skip(perPage * page)
         .sort('-createdOn')
     } else {
       count = await Expression.find({
         clautano: { $regex: ricerca, $options: 'i' },
-        $or: [{ 'user.isVerified': true }, { 'user.id': req.user ? req.user._id : undefined }]
+        $or: [
+          { 'user.isVerified': true },
+          { 'user.id': req.user ? req.user._id : undefined },
+        ],
       }).count()
       expressions = await Expression.find({
         clautano: { $regex: ricerca, $options: 'i' },
-        $or: [{ 'user.isVerified': true }, { 'user.id': req.user ? req.user._id : undefined }]
-      }).limit(perPage)
+        $or: [
+          { 'user.isVerified': true },
+          { 'user.id': req.user ? req.user._id : undefined },
+        ],
+      })
+        .limit(perPage)
         .skip(perPage * page)
         .sort('-createdOn')
     }
@@ -52,7 +62,7 @@ router.get('/search', async (req, res, next) => {
     expressions,
     page: page + 1,
     pages: Math.ceil(count / perPage),
-    ricerca
+    ricerca,
   })
 })
 
@@ -65,9 +75,7 @@ router.get('/new', isLoggedIn, async (req, res) => {
 router.post('/', isLoggedIn, async (req, res, next) => {
   const linkedWords = JSON.parse(req.body.expression.words)
   req.body.expression.words = linkedWords
-  const newExpression = new Expression(
-    req.body.expression
-  )
+  const newExpression = new Expression(req.body.expression)
   newExpression.user.id = req.user._id
   newExpression.user.fullName = req.user.fullName
   newExpression.user.isVerified = req.user.isVerified
@@ -83,17 +91,21 @@ router.post('/', isLoggedIn, async (req, res, next) => {
 // SHOW information about specific expression
 router.get('/:id', async (req, res, next) => {
   try {
-    const expression = await Expression.findById(req.params.id).populate('words').exec()
+    const expression = await Expression.findById(req.params.id)
+      .populate('words')
+      .exec()
     res.render('expressions/show', { expression })
   } catch (e) {
     next(e)
   }
 })
 
-// // EDIT show edit expression form
+// EDIT show edit expression form
 router.get('/:id/edit', isExpressionOwnerOrAdmin, async (req, res, next) => {
   try {
-    const expression = await Expression.findById(req.params.id).populate('words').exec()
+    const expression = await Expression.findById(req.params.id)
+      .populate('words')
+      .exec()
     res.render('expressions/edit', { expression })
   } catch (e) {
     next(e)
@@ -106,10 +118,14 @@ router.put('/:id', isExpressionOwnerOrAdmin, async (req, res, next) => {
   req.body.expression.words = linkedWords
 
   try {
-    const exp = await Expression.findByIdAndUpdate(req.params.id, req.body.expression, {
-      new: true,
-      runValidators: true
-    })
+    const exp = await Expression.findByIdAndUpdate(
+      req.params.id,
+      req.body.expression,
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
     if (!exp) {
       res.status(404).send('Word not found!')
     }
@@ -124,7 +140,10 @@ router.put('/:id', isExpressionOwnerOrAdmin, async (req, res, next) => {
 router.delete('/:id', isExpressionOwnerOrAdmin, async (req, res, next) => {
   try {
     const deleted = await Expression.findByIdAndDelete(req.params.id)
-    req.flash('success', 'Espressione "' + deleted.clautano + '" eliminata definitivamente.')
+    req.flash(
+      'success',
+      'Espressione "' + deleted.clautano + '" eliminata definitivamente.'
+    )
     res.redirect('/')
   } catch (e) {
     next(e)
